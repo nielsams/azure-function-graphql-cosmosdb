e3023820c6cb817caca17b451b6ec1df6dc7faea
Niels Buit <buit@outlook.com>
2022-04-13 16:33:13 +0200

    work in progress

--- src/api/graphql/index.ts
+++ src/api/graphql/index.ts
@@ -1,25 +1,78 @@
 import { ApolloServer, gql } from "apollo-server-azure-functions";
 import { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageDisabled } from 'apollo-server-core';
-import { CosmosClient } from "@azure/cosmos";
+import { CosmosClient, Item } from "@azure/cosmos";
 
 const typeDefs = gql`
     type ListItem {
+        id: ID,
+        list: List!
         Title: String,
         Status: String,
         CompletionDate: String
     }
 
-    type ToDoListQuery {
-        ListItems(listId: String!): [ListItem]
+    type List {
+        id: ID,
+        title: String
+        items: [ListItem]
     }
 
-    schema {
-        query: ToDoListQuery
+    type Query {
+        Lists: [List]
+        ListById(listId: String!): List
+        ListItems(listId: String!): [ListItem]
     }
 `;
 
 const resolvers = {
-    ToDoListQuery: {
+    List: {
+        async items(parent) {
+            let results = await client
+                .database("ToDoList")
+                .container("Items")
+                .items.query({
+                    query: "SELECT * FROM c WHERE c.listId=@listId",
+                    parameters: [
+                        {
+                            name: "@listId",
+                            value: parent.id
+                        }
+                    ]
+                })
+                .fetchAll();
+
+            return results.resources;
+        }
+    },
+    Query: {
+        async Lists(_) {
+            let results = await client
+            .database("ToDoList")
+            .container("Lists")
+            .items.query({
+                query: "SELECT * FROM c"
+            })
+            .fetchAll();
+
+        return results.resources; 
+        },
+        async ListById(_, {listId}: {listId:String}) {
+            let results = await client
+                .database("ToDoList")
+                .container("Lists")
+                .items.query({
+                    query: "SELECT * FROM c WHERE c.id=@listId",
+                    parameters: [
+                        {
+                            name: "@listId",
+                            value: listId
+                        }
+                    ]
+                })
+                .fetchAll();
+
+            return results.resources[0];
+        },
         async ListItems(_, {listId}: {listId:String}) {
             let results = await client
                 .database("ToDoList")
